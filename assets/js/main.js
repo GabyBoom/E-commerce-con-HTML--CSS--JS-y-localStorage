79f9bfc66bd3fc5da244c5bf2fde1301e45271bc
Gabriel <gaby_lamuerte@hotmail.com>
2023-09-09 15:24:47 -0300

    ya funciona el carrito

---
+++ assets/js/main.js
@@ -0,0 +1,169 @@
+// PRODUCTOS
+const productos = [
+    // Abrigos
+    {
+        id: "abrigo-01",
+        titulo: "Abrigo 01",
+        imagen: "./assets/img/empanada.PNG",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-02",
+        titulo: "Abrigo 02",
+        imagen: "./assets/img/Sandwich de mila.jpeg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-03",
+        titulo: "Abrigo 03",
+        imagen: "./assets/img/lomito.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-04",
+        titulo: "Abrigo 04",
+        imagen: "./assets/img/pizza.PNG",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-01",
+        titulo: "Abrigo 01",
+        imagen: "./assets/img/empanada.PNG",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-02",
+        titulo: "Abrigo 02",
+        imagen: "./assets/img/Sandwich de mila.jpeg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-03",
+        titulo: "Abrigo 03",
+        imagen: "./assets/img/lomito.jpg",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-04",
+        titulo: "Abrigo 04",
+        imagen: "./assets/img/pizza.PNG",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    }
+    
+]
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".botones-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+function cargarProductos(productosElegidos) {
+    
+    contenedorProductos.innerHTML = ""
+
+    productos.forEach(producto => {
+        productosElegidos.forEach(productoElegido => {
+            
+        })
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}" >
+            <div class="productos-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+`;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerHTML = productoCategoria.categoria.nombre;
+
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerHTML = "Todos los productos";
+            cargarProductos(productos);
+        }
+        
+    })
+});
+
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    })
+}
+
+const productosEnCarrito = [];
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton)
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+    
+    actualizarNumerito();
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerHTML = nuevoNumerito;
+}
